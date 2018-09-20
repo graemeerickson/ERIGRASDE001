927002444793c4c26032ee1432e791dc71ae64de
graemeerickson <graemeerickson@gmail.com>
2018-09-20 12:52:21 -0700

    Implemented loop to check each FB post's relevant content and store into a new object that will be outputted to a file

--- expedia_fb_posts.py
+++ expedia_fb_posts.py
@@ -12,5 +12,20 @@ fb_connection.close()
 # parse html using BeautifulSoup
 fb_page_soup = soup(fb_html, "html.parser")
 
-# get each FB post based on class name
+# find the first 8 FB posts based on element & class name
 posts = fb_page_soup.findAll("div", {"class": "_1dwg _1w_m _q7o"}, limit=MAX_POSTS)
+
+# declare a new object to hold details of each post
+postsObj = {}
+
+# loop through posts and find each post's relevant content based on element & class name, then store results into the posts object
+for index in range(len(posts)):
+  title = posts[index].find("span", {"class": "fcg"}).text
+  timestamp = posts[index].find("span", {"class": "timestampContent"}).text
+  text = posts[index].find("div", {"class": "_5pbx userContent _3576"}).text
+  
+  postsObj[index] = {
+    'title': title,
+    'timestamp': timestamp,
+    'text': text
+  }
