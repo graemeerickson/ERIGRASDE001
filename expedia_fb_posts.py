fba10a143b33b7159464e40a6311426079a2ecd2
graemeerickson <graemeerickson@gmail.com>
2018-09-20 12:34:02 -0700

    Use BeautifulSoup to parse the FB Posts page

--- expedia_fb_posts.py
+++ expedia_fb_posts.py
@@ -1,7 +1,11 @@
 from urllib.request import urlopen
+from bs4 import BeautifulSoup as soup
 
 # open connection to Expedia's Facebook posts page and fetch the page content
 expedia_fb_posts_url = 'https://www.facebook.com/pg/expedia/posts/'
 fb_connection = urlopen(expedia_fb_posts_url)
 fb_html = fb_connection.read()
 fb_connection.close()
+
+# parse html using BeautifulSoup
+fb_page_soup = soup(fb_html, "html.parser")
