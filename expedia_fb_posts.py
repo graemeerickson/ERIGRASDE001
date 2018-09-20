97911a8c96aeee33e49a905e001307c14dfae1f4
graemeerickson <graemeerickson@gmail.com>
2018-09-20 12:37:05 -0700

    Find all posts based on element & class name and store results into a variable

--- expedia_fb_posts.py
+++ expedia_fb_posts.py
@@ -1,6 +1,8 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as soup
 
+MAX_POSTS = 8
+
 # open connection to Expedia's Facebook posts page and fetch the page content
 expedia_fb_posts_url = 'https://www.facebook.com/pg/expedia/posts/'
 fb_connection = urlopen(expedia_fb_posts_url)
@@ -9,3 +11,6 @@ fb_connection.close()
 
 # parse html using BeautifulSoup
 fb_page_soup = soup(fb_html, "html.parser")
+
+# get each FB post based on class name
+posts = fb_page_soup.findAll("div", {"class": "_1dwg _1w_m _q7o"}, limit=MAX_POSTS)
