4c9144c219f2ac180ac1dbec69b7bc628b4e8585
graemeerickson <graemeerickson@gmail.com>
2018-09-23 13:29:14 -0700

    Updating terminology to align with Python standards (list v. array)

--- expedia_fb_posts.py
+++ expedia_fb_posts.py
@@ -16,18 +16,18 @@ fb_page_soup = soup(fb_html, "html.parser")
 # find the specified number of FB posts based on element & class name
 posts = fb_page_soup.findAll("div", {"class": "_1dwg _1w_m _q7o"}, limit=MAX_POSTS)
 
-# declare a new array to hold details of each post
-posts_arr = []
+# declare a new list to hold details of each post
+posts_list = []
 
-# loop through posts and find each post's relevant content based on element & class name, then store results into the posts object
+# loop through posts and find each post's relevant content based on element & class name, then store results into the posts list
 for index in range(len(posts)):
   # construct a dict containing relevant post data
   title = posts[index].find("span", {"class": "fcg"}).text
   timestamp = posts[index].find("span", {"class": "timestampContent"}).text
   text = posts[index].find("div", {"class": "_5pbx userContent _3576"}).text
   
-  # append dict to array of posts
-  posts_arr.append({
+  # append dict to list of posts
+  posts_list.append({
     'title': title,
     'timestamp': timestamp,
     'text': text
@@ -36,4 +36,4 @@ for index in range(len(posts)):
 # open or create a txt file, and use json & codecs modules to write the posts object to it
 # credit: https://stackoverflow.com/questions/12309269/how-do-i-write-json-data-to-a-file
 with open('expedia_fb_posts.txt', 'wb') as outfile:
-  json.dump(posts_arr, codecs.getwriter('utf-8')(outfile), ensure_ascii=False)
+  json.dump(posts_list, codecs.getwriter('utf-8')(outfile), ensure_ascii=False)
